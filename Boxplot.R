07cdef7e95384fab1c24d6f4fc7ca860d02d9312
Sandra <36429621+Sandythesan@users.noreply.github.com>
2022-06-10 14:22:57 -0500

    Update Boxplot.R

--- Boxplot.R
+++ Boxplot.R
@@ -1,2 +1,12 @@
 
 # Aquí pueden editar el archivpo
+boxedit<-function(df, col){
+  vectorname<-names(fb)[col]
+  boxplot(df[vectorname], col = "palegreen2",
+          notch = TRUE, # Add notch if TRUE,
+          ylab= vectorname,lty = 1)
+  title(vectorname)
+}
+
+# Ejemplo
+boxedit(fb, 1)
