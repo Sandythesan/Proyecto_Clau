17dd9acaac3e0eaa507b175ff489ffd291add3a6
Sandra <36429621+Sandythesan@users.noreply.github.com>
2022-06-10 11:37:18 -0500

    Create Proyecto_main.R

---
+++ Proyecto_main.R
@@ -0,0 +1,148 @@
+############
+#    Importación de los datos
+####
+library(RCurl)
+x <- getURL("https://raw.githubusercontent.com/EduardoSelimMM/ProyectosRegresion/main/datasets/facebook_cosmetic_data.csv")
+fb <- read.csv(text = x)
+library("scatterplot3d")
+library(MASS)
+##########
+#  Cambio para que estas variables sean factores
+####
+
+fb$Type<-as.factor(fb$Type)
+fb$Category<-as.factor(fb$Category)
+fb$Paid<-as.factor(fb$Paid)
+fb$Post.Month<-as.factor(fb$Post.Month)
+fb$Post.Weekday<-as.factor(fb$Post.Weekday)
+fb$Post.Hour<-as.factor(fb$Post.Hour)
+#########
+#    Aquí se puede hacer cualquier manejo de los missing data
+#           Yo lo que hice fue eliminarlos, eran pocos y por facilidad
+#                    más que nada para que lo demás corriera bien
+####
+as.data.frame(sapply(fb,function(x) sum(is.na(x))))
+fb2<-na.omit(fb)
+as.data.frame(sapply(fb2,function(x) sum(is.na(x))))
+fb<-fb2
+
+##########
+#  Se dan los nombres y los summary de los datos
+####
+
+names(fb)
+summary(fb)
+
+###############
+#    Las siguientes son gráficas para ver las correlaciones entre las var. de respuesta
+####
+pairs(~Lifetime.Post.Total.Reach+ Lifetime.Post.Total.Impressions+
+        Lifetime.Engaged.Users+Lifetime.Post.Consumers+
+        Lifetime.Post.Consumptions+Lifetime.Post.Impressions.by.people.who.have.liked.your.Page +
+        Lifetime.Post.reach.by.people.who.like.your.Page+
+        Lifetime.People.who.have.liked.your.Page.and.engaged.with.your.post+
+        comment+like+share+Total.Interactions, data = fb)
+
+"
+# De lo anterior es de notar que hay relaciones medio lineales entre:
+
+ Like y Total interactions
+ Life engaged users y Life post consumption
+"
+###############
+#    Las siguientes son gráficas para ver las correlaciones entre las var. de entrada
+##     Pero sólo se usaron las variables continuas, y se pintó de acuerdo a las var categóricas
+####
+
+
+pairs(~log(Lifetime.Post.Total.Reach)+Page.total.likes+Post.Month+Post.Weekday+Post.Hour,
+      col = factor(fb$Type), pch = 19, data=fb, main="Type")
+
+pairs(~Lifetime.Post.Total.Reach+Page.total.likes+Post.Month+Post.Weekday+Post.Hour,
+      col = factor(fb$Category), pch = 19, data=fb,main="Category")
+
+pairs(~Lifetime.Post.Total.Reach+Page.total.likes+Post.Month+Post.Weekday+Post.Hour,
+      col = factor(fb$Paid), pch = 19, data=fb, main="Paid")
+
+"
+Sin embargo, al menos yo, no observo nada muy claro.
+Si es de notar que la variable Total de likes tiene relación importante con el Mes
+   Eso es lógico pues los datos fueron del 2014, del 1 de enero al 31 de diciembre
+       es de esperarse que la página tenga más suscriptores con el tiempo.
+"
+
+###############
+#    Las siguientes son gráficas para ver las correlaciones dos variables continuas y la de salida
+#               Sigue siendo un análisis exploratorio para ver si existe correlación
+####
+
+model1<-lm(fb$Lifetime.Post.Total.Reach~fb$Post.Month+fb$Post.Weekday)
+scatterplot3d(fb$Post.Month,fb$Post.Weekday,
+              fb$Lifetime.Post.Total.Reach,
+              type = "h", color = "blue", pch = 16,
+              angle=150)$plane3d(model1)
+
+model2<-lm(fb$Lifetime.Post.Total.Reach~fb$Post.Hour+fb$Post.Weekday)
+scatterplot3d(fb$Post.Hour,fb$Post.Weekday,
+              fb$Lifetime.Post.Total.Reach,
+              type = "h", color = factor(fb$Category), pch = 16,
+              angle=40)$plane3d(model2)
+
+model3<-lm(fb$Lifetime.Post.Total.Reach~fb$Post.Hour+fb$Post.Month)
+scatterplot3d(fb$Post.Hour,fb$Post.Month,
+              fb$Lifetime.Post.Total.Reach,
+              type = "h", color = factor(fb$Category), pch = 16,
+              angle=30)$plane3d(model3)
+
+"
+No se vé que el modelo utilizando esas variables vaya a explicar mucho
+"
+
+"
+Para la parte que sigue se van a hacer varias cosas.
+
+1) Se hizo un modelo sólo utilizando las variables:
+Mes, día, hora, tipo, clase, pago
+
+Pensando que estas son variables que se pueden controlar.
+A este le llamamos el modelo pred, de predictivo
+
+--
+2) Se hizo un modelo con todas las variables. Se le llamó el modelo saturado
+
+___
+3) Se realizó el comando step para poder obtener el mejor modelo de los dos casos anteriores.
+
+"
+#############
+#    1) Modelo predictivo con su optimización
+####
+
+fb_pred<-lm(fb$Lifetime.Post.Total.Reach~fb$Post.Hour+fb$Post.Weekday+fb$Post.Month+fb$Type+fb$Category+fb$Paid)
+step.model_pred<-stepAIC(fb_pred, direction = "both",
+                         trace = TRUE)
+summary(step.model_pred)
+plot(step.model_pred)
+
+
+#############
+#    2) Modelo total, saturado, con todas las variables; y su optimización
+####
+
+
+fb_sat<-lm(log(Lifetime.Post.Total.Reach)~., fb)
+step.model_sat <- stepAIC(fb_sat, direction = "both", 
+                      trace = TRUE)
+summary(step.model_sat)
+plot(step.model_sat)
+
+
+#############
+#    4) Se realizó de nuevo el modelo predictivo, pero utilizando el log del alcance
+####
+
+fb_pred_log<-lm(log(fb$Lifetime.Post.Total.Reach)~fb$Post.Hour+fb$Post.Weekday+fb$Post.Month+fb$Type+fb$Category+fb$Paid)
+step.model_pred_log<-stepAIC(fb_pred_log, direction = "both",
+                             trace = TRUE)
+summary(step.model_pred_log) 
+plot(step.model_pred_log)
